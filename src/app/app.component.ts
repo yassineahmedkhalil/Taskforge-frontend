43ad5214e9fa0909e28369577a83039f8bbbd516
Ahmed Yassine <yassineahmedkhalil@gmail.com>
2026-02-04 16:29:48 +0100

    feature/ Login form with Username/Email support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthTogglePageComponent } from './auth/auth-toggle-page/auth-toggle-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthTogglePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
