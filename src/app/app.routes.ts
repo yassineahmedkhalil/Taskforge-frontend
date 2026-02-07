127653d2b475c2ce8c8272443956af4622e73fb2
Ahmed Yassine <yassineahmedkhalil@gmail.com>
2026-02-07 23:45:17 +0100

    feat(dashboard): create dashboard component + login redirect

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { AuthTogglePageComponent } from './auth/auth-toggle-page/auth-toggle-page.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: '/auth', pathMatch: 'full' },
-    { path: 'auth',component: AuthTogglePageComponent, loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES) }
-];
